01a064d6d7d11b5d2f08335c607497386908b10c
saisudhapanchadi <116145690+saisudhapanchadi@users.noreply.github.com>
2024-04-11 20:17:17 +0530

    Update Forecast.tsx

--- finished-result/src/components/Forecast.tsx
+++ finished-result/src/components/Forecast.tsx
@@ -19,7 +19,7 @@ type Props = {
 
 const Forecast = ({ data }: Props) => {
   const today = data.list[0]
-
+  
   return (
     <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg">
       <div className="mx-auto w-[300px]">
